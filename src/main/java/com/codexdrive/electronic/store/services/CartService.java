1f5ac44e5452bd9d6aa68f0eb9b0cd6357fc74ea
Chitranshu <chitranshu2411@gmail.com>
2026-09-24 21:10:08 +0530

    Define cart service contract for managing cart operations and business workflows
    Define cart service contract for managing cart operations and business workflows

---
+++ src/main/java/com/codexdrive/electronic/store/services/CartService.java
@@ -0,0 +1,24 @@
+package com.codexdrive.electronic.store.services;
+
+import com.codexdrive.electronic.store.dtos.AddItemToCartRequest;
+import com.codexdrive.electronic.store.dtos.CartDto;
+
+public interface CartService {
+
+    //add items to cart:
+    // case1: cart for user is not available: we will create the cart and then add the item
+    //case2: cart available add the items to cart
+
+    CartDto addItemToCart(String userId, AddItemToCartRequest request);
+
+    //remove item from cart:
+    void removeItemFromCart(String userId, int cartItem);
+
+    //remove all items from cart
+    void clearCart(String userId);
+
+
+    CartDto getCartByUser(String userId);
+
+
+}
