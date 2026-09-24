eb7badcb83129ab42dddde2178ca2266297a342d
Chitranshu <chitranshu2411@gmail.com>
2026-09-24 21:14:47 +0530

    Define category service contract for category management operations
    Define category service contract for category management operations

---
+++ src/main/java/com/codexdrive/electronic/store/services/CategoryService.java
@@ -0,0 +1,24 @@
+package com.codexdrive.electronic.store.services;
+
+import com.codexdrive.electronic.store.dtos.CategoryDto;
+import com.codexdrive.electronic.store.dtos.PageableResponse;
+
+public interface CategoryService {
+
+    //create
+    CategoryDto create(CategoryDto categoryDto);
+
+    //update
+     CategoryDto update(CategoryDto categoryDto , String categoryId);
+
+    //delete
+    void delete(String categoryId);
+
+    //get all
+    PageableResponse<CategoryDto> getAll(int pageNumber , int pageSize, String sortBy, String sortDir);
+
+    //get single category details
+    CategoryDto get(String categoryId);
+
+    //search:
+}
