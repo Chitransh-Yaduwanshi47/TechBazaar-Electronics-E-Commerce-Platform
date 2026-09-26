63fdf7ec4ce0c13976604fa2276bbc5804ffb769
Chitranshu <chitranshu2411@gmail.com>
2026-09-26 23:51:21 +0530

    add CreateOrderRequest for handling order creation requests
    add CreateOrderRequest for handling order creation requests

---
+++ src/main/java/com/codexdrive/electronic/store/dtos/CreateOrderRequest.java
@@ -0,0 +1,33 @@
+package com.codexdrive.electronic.store.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class CreateOrderRequest {
+
+    @NotBlank(message = "Card id is required !!")
+    private String cartId;
+
+    @NotBlank(message = "Card id is required !!")
+    private String userId;
+
+    private String orderStatus = "PENDING";
+    private String paymentStatus = "NOTPAID";
+    @NotBlank(message = "Address is required !!")
+    private String billingAddress;
+    @NotBlank(message = "Phone number is required !!")
+    private String billingPhone;
+    @NotBlank(message = "Billing name is required !!")
+    private String billingName;
+
+}
